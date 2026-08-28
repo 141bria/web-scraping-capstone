582c25defc8148ccc39d479bc72114a30bf6df9d
Brii <141.brii@gmail.com>
2026-08-28 16:10:01 -0500

    Complete web scraping and CSV data

--- weather_scraper.py
+++ weather_scraper.py
@@ -1,3 +1,5 @@
+import csv
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -5,9 +7,15 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.get("https://www.timeanddate.com/weather/glossary.html")
-gwords = driver.find_element(By.CSS_SELECTOR,'["gwords"]')
-definitions = gwords.find_elements(By.CSS_SELECTOR,'[id="A"]')
-for word in definitions:
-    one_word = driver.find_element(By.TAG_NAME,"dl")
-    weather_words ={"Words":word.text,"Definitions":definitions}
-weather = []
+gwords = driver.find_element(By.CSS_SELECTOR,"#gwords")
+definitions = gwords.find_elements(By.CSS_SELECTOR,"dd")
+words= gwords.find_elements(By.TAG_NAME,"dt")
+with open("weather_words.csv","w",newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Word","Definition"])
+    for word, definition in zip(words,definitions):
+        writer.writerow([word.text,definition.text])
+
+driver.quit()
+
+pd.read_csv("weather_words.csv")
